5489441338e1f60016170007dc7b653bccd91dae
Farlinder <sashakost2006e@gmail.com>
2026-03-04 20:53:23 +0100

    Solving

--- services/movie_session.py
+++ services/movie_session.py
@@ -6,7 +6,7 @@ from db.models import MovieSession, Ticket
 
 
 def create_movie_session(
-        movie_show_time: datetime.datetime,
+        movie_show_time: datetime,
         movie_id: int,
         cinema_hall_id: int,
 ) -> MovieSession:
